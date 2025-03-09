d6b87f6c03f4424ff1d32f6cc26041602235063b
TenzinPlatter <143778894+TenzinPlatter@users.noreply.github.com>
2025-03-09 15:48:01 +1100

    Added cli arg flag to specify mic

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,11 @@
 pub mod mic_utils;
+
+pub fn use_default_mic() -> bool {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        return true;
+    } else {
+        // if passing -d flag then don't use default mic
+        args[1].trim() != "-d"
+    }
+}
