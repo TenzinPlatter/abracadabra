3e3dc81717a3124f25faccefd37e3b94f000a611
Tenzin Platter <143778894+TenzinPlatter@users.noreply.github.com>
2025-03-11 21:40:56 +1100

    Created windowing and fft functions

--- src/audio_processing.rs
+++ src/audio_processing.rs
@@ -1,142 +1,49 @@
 use std::error::Error;
 
-use apodize::hamming_iter;
-use rustfft::{FftPlanner, num_complex::Complex};
-
-pub struct WindowFreqInfo {
+/// time offset is in ms
+///
+struct WindowFrequencyInfo {
     pub time_offset: u64,
     pub frequencies: Vec<FrequencyInfo>,
 }
 
-pub struct FrequencyInfo {
-    pub freq: u64,
-    pub intensity: f32,
+struct FrequencyInfo {
+    pub hertz: u64,
+    pub intensity: f64,
 }
 
-pub struct MagnitudeInfo {
-    pub intensity: f32,
-    pub time_offset: u64,
-}
-
-pub fn magnitude_data_to_frequency_data(
-    magnitude_data: Vec<Vec<MagnitudeInfo>>,
-    sample_rate: u32,
-    bin_size: usize,
-) -> Vec<WindowFreqInfo> {
-    let mut windows: Vec<WindowFreqInfo> = Vec::with_capacity(magnitude_data.len());
-
-    for section in magnitude_data {
-        let mut window: Vec<FrequencyInfo> = Vec::with_capacity(section.len());
-        let time = section.first().unwrap().time_offset;
-
-        for (i, bin) in section.iter().enumerate() {
-            let freq = i as u64 * sample_rate as u64 / bin_size as u64;
-
-            window.push(FrequencyInfo {
-                freq,
-                intensity: bin.intensity.clone(),
-            });
-        }
-
-        windows.push(WindowFreqInfo {
-            time_offset: time,
-            frequencies: window,
-        });
-    }
-
-    windows
-}
-
-pub fn no_window_mag(
+pub fn process_audio(
     samples: &mut Vec<f32>,
-    window_size: usize,
-    overlap: f32,
     sample_rate: u32,
-) -> Result<Vec<MagnitudeInfo>, Box<dyn Error>> {
-    let mut fft_buf: Vec<Complex<f32>> = Vec::with_capacity(samples.len());
+) -> Result<Vec<(f64, f64)>, Box<dyn Error>> {
+    let windows = split_data_into_windows(samples, 2048, 0.8);
+    let hanned = spectrum_analyzer::windows::hann_window(samples);
+    let spectrum = spectrum_analyzer::samples_fft_to_spectrum(
+        &windowed,
+        sample_rate,
+        spectrum_analyzer::FrequencyLimit::All,
+        Some(&spectrum_analyzer::scaling::divide_by_N),
+    )
+    .unwrap()
+    .data()
+    .iter()
+    .map(|(freq, mag)| (freq.val() as f64, mag.val() as f64))
+    .collect();
 
-    let mean: f32 = samples.iter().sum::<f32>() / samples.len() as f32;
-    let mut hamming = hamming_iter(samples.len());
-
-    for sample in samples.iter() {
-        // sample - mean to remove DC bias
-        fft_buf.push(Complex {
-            re: (*sample - mean) * hamming.next().unwrap() as f32,
-            im: 0.,
-        });
-    }
-
-    compute_fft(&mut fft_buf);
-
-    let mut window_magnitudes = Vec::with_capacity(samples.len());
-
-    for complex in fft_buf[..fft_buf.len() / 2].iter() {
-        window_magnitudes.push(MagnitudeInfo {
-            intensity: complex.norm(),
-            time_offset: 0,
-        });
-    }
-
-    Ok(window_magnitudes)
+    Ok(spectrum)
 }
 
-/// processes audio from samples to windowed frequency data
-/// window_size --> number of samples for each window
-/// overlap --> percentage overlap between samples, should be between 0-1
-/// for music processing 75 - 87.5 is good
-/// Return --> Vec<Vec<(magnitude, timeoffset)>>
-pub fn samples_to_magnitude_data_windows(
+/// Splits samples into windows so that when applying fft, frequency data is related to time
+/// window_size is in samples
+/// overlap should be between 0-1 where 0 is no overlap and 1 is completely overlapping
+fn split_data_into_windows(
     samples: &mut Vec<f32>,
-    window_size: usize,
+    window_size: u32,
     overlap: f32,
-    sample_rate: u32,
-) -> Result<Vec<Vec<MagnitudeInfo>>, Box<dyn Error>> {
-    let step_size: usize = (window_size as f32 * (1. - overlap)).floor() as usize;
-    let mut windows: Vec<Vec<MagnitudeInfo>> = Vec::with_capacity(samples.len() / window_size);
-
-    let mut start: usize = 0;
-    while start + window_size <= samples.len() {
-        let window = &samples[start..start + window_size];
-
-        let mut fft_buf: Vec<Complex<f32>> = Vec::with_capacity(window.len());
-
-        let mean: f32 = window.iter().sum::<f32>() / window.len() as f32;
-
-        for (i, sample) in window.iter().enumerate() {
-            // sample - mean to remove DC bias
-            let sample = (*sample - mean)
-                * (0.5
-                    * (1. - f32::cos(2. * std::f32::consts::PI * i as f32 / window.len() as f32))
-                        as f32);
-
-            fft_buf.push(Complex { re: sample, im: 0. });
-        }
-
-        compute_fft(&mut fft_buf);
-
-        let mut window_magnitudes = Vec::with_capacity(window.len());
-
-        // cut in half for nyquist limit
-        for (i, complex) in fft_buf[..fft_buf.len() / 2].iter().enumerate() {
-            // offset in seconds from beginning of audio sample
-            let time = i as u64 * step_size as u64 / sample_rate as u64;
-
-            window_magnitudes.push(MagnitudeInfo {
-                intensity: complex.norm(),
-                time_offset: time,
-            });
-        }
-
-        windows.push(window_magnitudes);
-
-        start += step_size;
+) -> Result<Vec<Vec<f32>>, Box<dyn Error>> {
+    if overlap >= 1 || overlap < 0 {
+        return Err("Please give an overlap value between < 1 and >= 0".into());
     }
 
-    Ok(windows)
-}
-
-fn compute_fft(buffer: &mut Vec<Complex<f32>>) {
-    let mut planner = FftPlanner::new();
-    let fft = planner.plan_fft_forward(buffer.len());
-    fft.process(buffer);
+    Ok(vec![])
 }
